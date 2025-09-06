e46917aa3b8a7a0a93a3be9973cc0a6fc15076e3
ofsevim <18220131002@ogr.ksu.edu.tr>
2025-09-06 03:32:52 +0300

    c

--- src/App.tsx
+++ src/App.tsx
@@ -78,14 +78,14 @@ const AppContent: React.FC = () => {
     <Layout onSearch={handleSearch} onShowNewAd={() => setShowNewAdModal(true)}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         {/* Main Content */}
-        <div className="grid grid-cols-1 lg:grid-cols-5 gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-6 gap-8">
           {/* Left Sidebar - Filters */}
           <div className="lg:col-span-1">
             <SidebarFilters filters={filters} onFiltersChange={setFilters} />
           </div>
 
           {/* Right Content - Ads */}
-          <div className="lg:col-span-4">
+          <div className="lg:col-span-5">
             {/* Results Header */}
             <div className="flex items-center justify-between mb-6">
               <h2 className="text-xl font-semibold text-gray-900 dark:text-white">
